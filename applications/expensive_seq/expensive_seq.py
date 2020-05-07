b394b1607212073e39c183699b0eaa5a7185e4d2
newbi462 <isthisemailfree@gmail.com>
2020-05-07 16:20:41 -0400

    Expensive Sequence

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,5 +1,20 @@
+speed = {}
 def expensive_seq(x, y, z):
     # Implement me
+    key = str(x) + str(y) + str(z)
+    try:
+        cashed_v = speed[key]
+        #print(speed)
+        return cashed_v
+    except KeyError:
+        if x <= 0:
+            v = y + z
+            speed[key] = v
+            return v
+        if x >  0:
+            v = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+            speed[key] = v
+            return v
 
 if __name__ == "__main__":
     for i in range(10):
